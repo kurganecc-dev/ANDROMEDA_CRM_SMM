a52a2ca9d327d613e358ec609f8af687613be4b3
kurganecc-dev <kurganecc@gmail.com>
2026-04-25 19:44:42 +0500

    Delete assets directory

--- assets/js/core/supabase.js
+++
@@ -1,8 +0,0 @@
-window.AppSupabase = (() => {
-  const SUPABASE_URL = 'https://wysiuvhhetjqfhpcykgm.supabase.co';
-  const SUPABASE_ANON_KEY = 'sb_publishable_PdY082TJszfAELdPBYsJvg_gJnFysNr';
-
-  const client = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
-
-  return { client };
-})();
